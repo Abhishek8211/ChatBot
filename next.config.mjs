a13f94485879e50d97a988a34d9764d86a258664
{Abhishek8211} <abhishekkrbgp8211@gmail.com>
2026-02-22 01:09:28 +0530

    js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: false,
+  experimental: {
+    optimizePackageImports: [
+      "react-icons",
+      "react-icons/hi",
+      "react-icons/hi2",
+      "framer-motion",
+      "recharts",
+      "react-countup",
+      "react-hot-toast",
+    ],
+  },
+};
 
 export default nextConfig;
